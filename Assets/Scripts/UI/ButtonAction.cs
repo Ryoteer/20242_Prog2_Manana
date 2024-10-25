c67ff925a466080b5e857af92b7d1dbf8e669f94
Ryoteer <72623439+Ryoteer@users.noreply.github.com>
2024-10-25 10:47:25 -0300

    Clase 11
    LOD Group; Carga asincrónica

---
+++ Assets/Scripts/UI/ButtonAction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+    public void LoadScenAsync(string scene)
+    {
+        SceneLoadManager.Instance.LoadSceneAsync(scene);
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
